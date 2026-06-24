5c5f198279efd9e262b751274078d175bdaa057a
Pablo Henrique <pblohasantos@gmail.com>
2026-06-24 13:47:25 -0300

    Alterando o JDK e voltando no GITHUB
    Fiz uma alteração do JDK utilizado no projeto passando para LTS mais atual e além disso soltando commit no repositório atual.

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 import java.util.Locale;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
-
-
-
-
-        System.out.printf("Dando commit e subindo o programa!!");
-        System.out.println("Será se agora a carreira em Back-end vem?");
+        System.out.println("Bora fazer ukma alteração so pra lembrar como fazer no github desktop");
 
     }
 }

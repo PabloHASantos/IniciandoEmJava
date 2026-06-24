69cbb38f1e0efc3574245ea95e7f3445dceb0a4d
Pablo Henrique <pblohasantos@gmail.com>
2026-06-24 13:49:44 -0300

    Mais uma alteração básica

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bora fazer ukma alteração so pra lembrar como fazer no github desktop");
+        System.out.println("Fazendo só mais um commit para saber se consigo enviar pelo console do próprio intellij");
 
     }
 }

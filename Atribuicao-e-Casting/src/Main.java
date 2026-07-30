657585f0151d1e3e85754c4c0b49b47e8ba0def3
Pablo Henrique <pblohasantos@gmail.com>
2026-07-30 13:41:36 -0300

    Cast e atribuição

---
+++ Atribuicao-e-Casting/src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+
+        int x, y;
+        double resultado;
+
+        x = 5;
+        y = 2;
+
+        resultado = (double) x / y;
+
+
+        IO.println(resultado);
+
+    }
+

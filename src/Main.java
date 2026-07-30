0ec7884ddff8fafc3298f9a20a120b82e5734e16
Pablo Henrique <pblohasantos@gmail.com>
2026-07-30 13:24:19 -0300

    Organizando projeto
    Separando projeto de acordo às aulas.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.util.Locale;
-import java.util.Scanner;
 
 
-public class Main {
-    public static void main(String[] args) {
+    void main() {
 
         String product1 = "Computer";
         String product2 = "Office desk";
@@ -16,16 +14,15 @@ public class Main {
         double price2 = 650.50;
         double measure = 53.234567;
 
-        System.out.println("Products:");
+        IO.println("Products:");
         System.out.printf("%s, which price is %.2f%n", product1, price1);
         System.out.printf("%s, which price is %.2f%n", product2, price2);
-        System.out.println();
+        IO.println();
         System.out.printf("Record: %d years old, code %d and gender %c%n",  age, code, gender);
-        System.out.println();
+        IO.println();
         System.out.printf("Measure with eight decimal places: %.8f%n", measure);
         System.out.printf("Rounde (Three decimal places): %.3f%n", measure);
         Locale.setDefault(Locale.US);
         System.out.printf("US decimal point: %.3f%n", measure);
 
     }
-}

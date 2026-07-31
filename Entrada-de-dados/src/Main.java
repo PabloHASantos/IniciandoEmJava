0d8fd5b233179821570a5bd5e15eb7770f23f964
Pablo Henrique <pblohasantos@gmail.com>
2026-07-31 13:23:47 -0300

    Entrada de dados com scanner

--- Entrada-de-dados/src/Main.java
+++ Entrada-de-dados/src/Main.java
@@ -1,13 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.Scanner;
+import java.util.Locale;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+void main() {
+
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
+
+    int x;
+    String s1, s2, s3;
+
+    x = sc.nextInt();
+    sc.nextLine();
+    s1 =  sc.nextLine();
+    s2 =  sc.nextLine();
+    s3 =  sc.nextLine();
+
+    IO.println("Dados digitados: ");
+    IO.println(x);
+    IO.println(s1);
+    IO.println(s2);
+    IO.println(s3);
+    sc.close();
 }
+

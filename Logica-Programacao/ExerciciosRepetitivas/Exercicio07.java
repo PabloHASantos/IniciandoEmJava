d198f74264ab7283fc63eb5e2414b382cd125227
Pablo Henrique <pblohasantos@gmail.com>
2026-09-22 12:40:54 -0300

    Exercicio 07

---
+++ Logica-Programacao/ExerciciosRepetitivas/Exercicio07.java
@@ -0,0 +1,11 @@
+package ExerciciosRepetitivas;
+import java.util.Scanner;
+
+public class Exercicio07 {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+
+        sc.close();
+    }
+}

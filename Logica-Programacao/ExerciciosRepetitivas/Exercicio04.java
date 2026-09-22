fd9ae5a623d0e071b6259b144459be155274791e
Pablo Henrique <pblohasantos@gmail.com>
2026-09-22 12:56:11 -0300

    Revisão
    Revisão da matéria após ficar um tempo parado.

--- Logica-Programacao/ExerciciosRepetitivas/Exercicio04.java
+++ Logica-Programacao/ExerciciosRepetitivas/Exercicio04.java
@@ -5,6 +5,7 @@ public class Exercicio04 {
     static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite um numero: ");
         int x = sc.nextInt();
         for(int i=1; i<=x; i++){
             if(i % 2 != 0){
